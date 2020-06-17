31072d4d800615c76cdef5c574707721019f4bdd
mrnev <mr.nevinskiy@gmail.com>
2020-06-17 20:34:46 +0300

    HW6-7

---
+++ src/main/java/HW6/Task1.java
@@ -0,0 +1,29 @@
+package HW6;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+
+public class Task1 {
+
+    private static final Logger log = Logger.getLogger(Task1.class);
+
+    public static void main(String[] args) {
+        final int[] res2 = process(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+        System.out.println(res2);
+    }
+
+    public static int[] process(int[] arrIn) {
+        int[] backArr = new int[2];
+        if(arrIn.length == 0) {
+            log.info("В массиве нет элементов");
+            return arrIn;
+        }
+        for (int i = arrIn.length - 3; i >= 0 ; i--) {
+            if(arrIn[i] == 4) {
+                System.arraycopy(arrIn, (i+1), backArr,0,2);
+                return backArr;
+            }
+        }
+        throw new RuntimeException("В массиве нет ни одной 4-ки после которой были бы числа");
+    }
+}
---
+++ src/main/java/HW7/TestException.java
@@ -0,0 +1,5 @@
+package HW7;
+
+class TestException extends Exception {
+    TestException() {}
+}
